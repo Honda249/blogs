91dd2110a56b586dc6926274a870e58049e32995
Honda249 <138697278+Honda249@users.noreply.github.com>
2024-01-26 23:22:53 +0300

    Create Btn.vue

---
+++ components/Btn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button type="submit" class="text-gray bg-teal-100 hover:bg-teal-400 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full sm:w-auto px-12 py-3 text-center dark:bg-blue-600 dark:hover:bg-teal-700 dark:focus:ring-teal-600">
+            {{ title }}
+        </button>
+    </div>
+</template>
+
+<script setup>
+    const { title } = defineProps(['title'])
+
+</script>
+
+<style scoped>
+
+</style>
